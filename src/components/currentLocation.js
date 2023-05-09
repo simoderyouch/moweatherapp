2e862a25abb938ab6081d0fce5ec9c15c055333c
simoderyouch <simoederyouch@outlook.com>
2023-05-09 19:56:50 +0100

    handle weather load and error

--- src/components/currentLocation.js
+++ src/components/currentLocation.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 export const currentLocation = () => {
     return new Promise((resolve, reject) => {
         if (navigator.geolocation) {
@@ -19,25 +17,3 @@ export const currentLocation = () => {
         }
     });
 };
-=======
-
-export const currentLocation = () => {
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    resolve({
-                        lat: position.coords.latitude,
-                        lon: position.coords.longitude,
-                    });
-                },
-                (error) => {
-                    reject(error);
-                }
-            );
-        } else {
-            reject(new Error("Geolocation is not supported by your browser."));
-        }
-    });
-};
->>>>>>> origin/version2
